2904107648ca24c7cc2424570f765f51b1e6051a
yunkwanghun <denidaschool@gmail.com>
2025-08-01 14:50:43 +0900

    update

--- arealife_page.py
+++ arealife_page.py
@@ -1,4 +1,10 @@
 import tkinter as tk
 
 class AreaLifePage(tk.Frame):
-    
+    def __init__(self, parent, board, user):
+        tk.Frame.__init__(self, parent)
+        self.board = board
+        self.user = user
+
+        # body_frame 만들기
+        self.body_frame = tk.Frame(self.parent, bg = "white")
