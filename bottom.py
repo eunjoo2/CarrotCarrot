ed9452e8907cd278ca7cc7eaea7ff233db53620c
Eun ju <ahert3366@naver.com>
2025-08-01 09:40:22 +0900

    merge

---
+++ bottom.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class bottom (tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master, bg="#FF6F0F", bottom=30)
+        self.pack(fill="x")
+        self.create_widgets()
+
+    def create_widgets(self):
+        title = tk.Label(self, text="당근마켓", fg="white", font=("Arial", 16, "bold"))
+        title.pack(padx=10, pady=10, anchor="w")
